88113615659d78a8a80c83e6e7e9f5d13afc5ac1
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-04-20 18:14:59 +0530

    basics of server.js

---
+++ BACKEND/server.js
@@ -0,0 +1,36 @@
+//Import dependancies
+const express =  require("express");
+const mongoose = require("mongoose");
+const bodyPaser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070;//assigning available port address
+
+//use dependencies
+app.use(cors());
+app.use(bodyPaser.json());
+
+const URL = process.env.MONGODB_URL;
+
+//make connection with db
+mongoose.connect(URL,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnfiedTopology: true,
+    useFindTopology: false
+});
+
+//open the connection
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb Connection success!");
+})
+
+//listen to the PORT
+app.listen(PORT, () => {
+    console.log("Server is up and running on port number: ${PORT}")
+})
+
