cf819f5ae408a56cf0ca21afdd7e2a98c2a492b0
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-05-01 17:01:23 +0530

    finished backend routes and testing

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -5,12 +5,12 @@ let Student = require("../models/Student");
 router.route("/add").post((req,res)=>{ //pass to the mogodb database
 
     const name = req.body.name;
-    const age = Number(req.vody.age);
+    const age = Number(req.body.age);
     const gender = req.body.gender;
 
     const newStudent = new Student({ 
         name,
-        sge,
+        age,
         gender
     })
     newStudent.save().then(()=>{
@@ -47,7 +47,7 @@ router.route("/update/:id").put(async(req,res) =>{
 
     const update = await Student.findByIdAndUpdate(userId,updateStudent)
     .then(()=>{
-        res.status(200).send({status:"user updated", user:update})
+        res.status(200).send({status:"user updated"})
     }).catch((err) =>{
         console.log(err);
         res.status(500).send({status: "Error with updating data", error:err.message});
@@ -68,9 +68,18 @@ router.route("/delete/:id").delete(async (req,res)=>{
 })
 
 //Get only one student details
-
+router.route("/get/:id").get(async (req,res)=>{
+    let userId = req.params.id;
+    const user = await Student.findById(userId)
+    .then((student)=>{
+        res.status(200).send({status: "User fetched" ,student})
+    }).catch((err)=>{
+        console.log(err.message);
+        res.status(500).send({status: "Error with get user", error: err.message});
+    })
+})
 
 module.exports = router;
 
 
-
+//then we need to backend test before goint to the front end
